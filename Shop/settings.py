acfa4fa47d8464e193c6cdc560053ee2203ba519
VladyslavPonomaryov <vladyslav.ponomaryov@ab-inbev.com>
2022-08-10 20:47:22 +0200

    remove unneded requirements

--- Shop/settings.py
+++ Shop/settings.py
@@ -113,10 +113,10 @@ USE_TZ = False
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+   os.path.join(BASE_DIR, "stat"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
